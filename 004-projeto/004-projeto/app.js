4e8dd6023d8c9c0cc36da8aac430907246f4d376
Lucas Eduardo Chrysostomo <90626838+Chrys1315@users.noreply.github.com>
2022-08-15 21:06:39 -0300

    Add files via upload

---
+++ 004-projeto/004-projeto/app.js
@@ -0,0 +1,25 @@
+var num1 = 5;
+var num2 = 2;
+
+var num3;
+
+
+
+console.log(num1, num2, num3);
+
+var num3 = (num1 / num2);
+
+console.log(num3);
+
+var num4 = num1 % num2;
+
+var num3_round = Math.round(num3);
+
+console.log(num3_round);
+
+var num5 = 1231123132312.65465498;
+console.log(num5);
+
+num5 = num5.toFixed(2);
+
+console.log(num5);
